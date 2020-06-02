b3519abd0ccf311073a2e8cfe54f1faf565ae7a5
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-02 15:11:19 -0300

    Add Tables: items and point_items

--- Backend/src/database/migrations/01_create_items.ts
+++ Backend/src/database/migrations/01_create_items.ts
@@ -1,7 +1,14 @@
-export async function up(){
+import Knex from 'knex';
 
+//TS tipagem do knex: falo qual é o formato da variável e tenho acesso a td inteligencia da ide
+export async function up(knex: Knex) {
+  return knex.schema.createTable('items', table => {
+    table.increments('id').primary();
+    table.string('image').notNullable();
+    table.string('title').notNullable();
+  });
 }
 
-export async function down(){
-  
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('items');
 }
--- Backend/src/database/migrations/02_create_point_items.ts
+++ Backend/src/database/migrations/02_create_point_items.ts
@@ -1,7 +1,20 @@
-export async function up(){
+import Knex from 'knex';
 
+//TS tipagem do knex: falo qual é o formato da variável e tenho acesso a td inteligencia da ide
+export async function up(knex: Knex) {
+  return knex.schema.createTable('point_items', table => {
+    table.increments('id').primary();
+    table.integer('point_id') //td id en pint_id tem q ser um id válido na tabela points
+      .notNullable()
+      .references('id') //2 no campo ID
+      .inTable('points'); //1 onde será criado a chave extrangeira, na tabela: Points
+    table.integer('item_id')
+      .notNullable()
+      .references('id')
+      .inTable('items');
+  });
 }
 
-export async function down(){
-  
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('point_items');
 }
