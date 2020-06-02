6da1df9042212b5eba6d2729ea8a9b72781165d7
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-02 18:11:54 -0300

    Add GET items with title and image_url

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import knex from './database/connection'; // Connection witd Database
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ messege: "Hello World" });
-});
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*'); //ele necesita tempo p regressar a info, entao await
+  // é igual: SELECT * FROM items
+
+  //Processo de transformar/tradução da informações -> Serialização
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  });
+
+  return response.json(serializedItems);
+}); 
 
 export default routes;
