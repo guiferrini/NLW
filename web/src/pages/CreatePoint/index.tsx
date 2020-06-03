be49be95a6358819a0d4d2cbdebfa175ca75a7d6
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-03 18:50:47 -0300

    Add to CreatePoint Lint to 'Home'.

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
   return (
-    <h1>Creat Point</h1>
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="logo"/>
+
+        <Link to='/'>
+          <FiArrowLeft />
+          Voltar para Home
+        </Link>
+      </header>
+
+      <form action=""></form>
+    </div>
   );
 };
 
