9f5e6140efb36f8fdf2fcc32de36d38a395a2cfa
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-03 11:19:35 -0300

    Add show one point: join - point_items.point_id

--- Backend/src/controllers/PointsController.ts
+++ Backend/src/controllers/PointsController.ts
@@ -14,7 +14,21 @@ class PointsController {
       return response.status(400).json({ message: 'Point not found.' })
     }
 
-    return response.json(point);
+    /**
+     retornando tds items q estão relacionados com os Pontos
+     listando tds os items q tem relação com esse pto de coleta
+     quero fazer um join na tabela (point_items) onde id do meu item (item_id) seja = point_items.item_id, seja = id q recebo em cima
+    
+     * SELECT *   FROM items
+     *   JOIN point_items ON items.id = point_items.item.id
+     *  WHERE point_items.point_id = { id }
+     */
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title')
+
+    return response.json({ point, items });
   }
 
   async create(resquest: Request, response: Response) {
