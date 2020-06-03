78e8a55891b3f51910ea38e3c3042d13273de22c
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-03 18:18:47 -0300

    Add Link to '/create-point'.

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -7,7 +7,7 @@ import CreatePoint from './pages/CreatePoint';
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Route component={Home} path='/' exact/>
+      <Route component={Home} path='/' exact  />
       <Route component={CreatePoint} path='/create-point' />
     </BrowserRouter>
   )
