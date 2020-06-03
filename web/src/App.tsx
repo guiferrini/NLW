d41bbfa7b0b6d488e264441e2f80318054eea475
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-03 17:52:11 -0300

    Add to Home: index and styles. Add global styles.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home';
+
 function App() {
+  
   return (
-      <h1>Hello World :)</h1>
+    <Home />  
   );
 }
 
 export default App;
+ 
