e026d859a5418e0d8ec0de55d55b15a170dcb416
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-04 09:39:18 -0300

    Add to CreatePoint index: MAP with leaflet and react-leaflet

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -59,6 +60,13 @@ const CreatePoint = () => {
               <h2>Endereço</h2>
               <span>Selecione o endereço no mapa</span>
             </legend>
+
+            <Map center={[-23.0436052, -46.9780466]} zoom={15.75}> 
+              <TileLayer 
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+            </Map>
 
             <div className="field-group">
               <div className="field">
