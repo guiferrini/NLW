45361c00e3395f258fcea74f03dc5be74dd5e1b8
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-06 15:10:17 -0300

    Add validation (routes.ts) on Backend with celebrate

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -1,4 +1,5 @@
 import express, { response, request } from 'express';
+import { celebrate, Joi } from 'celebrate';
 
 import multer from 'multer'; //biblioteca de upload
 import multerConfig from './config/multer'; //configurações da biblioteca
@@ -18,6 +19,24 @@ routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', upload.single('image') ,pointsController.create); //upload.1 unico arquivo('nome q recebe')
+routes.post(
+  '/points', 
+  upload.single('image'), //upload.1 unico arquivo('nome q recebe')
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(3).max(40).required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(), 
+    })
+  }, {
+    abortEarly: false 
+  }),
+  pointsController.create
+); 
 
 export default routes;
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -3,6 +3,7 @@ import express, { request, response } from 'express';
 import routes from './routes';
 import path from 'path';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = 3333;
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(routes);   
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors()); //lida c a forma q retornamos os erros p o frontend
 
 app.listen(PORT, () => {
   console.log('Server connected ✅');
