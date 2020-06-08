8b349ab9457a704da350d8a8329e975239db59cd
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-08 18:18:55 -0300

    Add validation at CreatePoint to uf

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -61,6 +61,7 @@ const CreatePoint = () => {
   const [validacaoWhatsapp, setValidacaoWhatsapp] = useState(''); //validar email input
   const [validacaoItems, setValidacaoItems] = useState(''); //validar seleção de no min 1 item
   const [validacaoLatLon, setValidacaoLatLon] = useState(''); //validar longitude e latitude
+  const [validacaoUf, setValidacaoUf] = useState(''); //validar se foi selecionado UF
 
   const history = useHistory();
 
@@ -162,17 +163,17 @@ const CreatePoint = () => {
       //esta voltando string - terminar validação
       
     const uf = selectedUf;
+
+    //validação uf
+    if (uf === '0') {
+      setValidacaoUf('Favor selecioana o Estado(UF)');
+    } else {
+      setValidacaoUf('');
+    }
+
+
     const city = selectedCity;
     const [latitude, longitude] = selectedPosition;
-    const items = selectedItems;
-
-    const data = new FormData();
-    
-    data.append('name', name);
-    data.append('email', email);
-    data.append('whatsapp', whatsapp);
-    data.append('uf', uf);
-    data.append('city', city);
 
     //validando longitude e latidude dif de zero
     if (latitude === 0 || longitude === 0) {
@@ -181,8 +182,7 @@ const CreatePoint = () => {
       setValidacaoLatLon('');
     }
 
-    data.append('latitude', String(latitude));
-    data.append('longitude', String(longitude));
+    const items = selectedItems;
 
     //validando items, obrigatorio selecionar
     if (items[0] == null) {
@@ -191,6 +191,16 @@ const CreatePoint = () => {
     } else {
       setValidacaoItems('');
     }
+
+    const data = new FormData();
+    
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
     data.append('items', items.join(','));
 
     if (selectedFile) { //tem q criar condição, pois selectedFile pode ser nulo/vazio
@@ -285,6 +295,7 @@ const CreatePoint = () => {
                     <option key={uf} value={uf}>{uf}</option>
                   ))}
                 </select>
+                { validacaoUf && <div className="erro">{validacaoUf}</div> }
               </div>
               <div className="field">
                 <label htmlFor="city">Cidade</label>
